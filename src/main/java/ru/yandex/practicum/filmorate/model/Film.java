031b8e7d944e021c6b3ffe2b4ba00342c866b6fc
groth50 <shkolotron5@yandex.ru>
2023-02-08 19:58:56 +0300

    feat: add likes functional

--- src/main/java/ru/yandex/practicum/filmorate/model/Film.java
+++ src/main/java/ru/yandex/practicum/filmorate/model/Film.java
@@ -7,6 +7,8 @@ import javax.validation.constraints.NotBlank;
 import javax.validation.constraints.NotNull;
 import javax.validation.constraints.Positive;
 import java.time.LocalDate;
+import java.util.HashSet;
+import java.util.Set;
 
 @Data
 public class Film {
@@ -20,4 +22,6 @@ public class Film {
     private LocalDate releaseDate;
     @Positive
     private int duration;
+
+    private Set<Long> likes = new HashSet<>();
 }
--- src/main/java/ru/yandex/practicum/filmorate/service/FilmService.java
+++ src/main/java/ru/yandex/practicum/filmorate/service/FilmService.java
@@ -2,9 +2,13 @@ package ru.yandex.practicum.filmorate.service;
 
 import org.springframework.stereotype.Service;
 import ru.yandex.practicum.filmorate.model.Film;
+import ru.yandex.practicum.filmorate.model.User;
 import ru.yandex.practicum.filmorate.storage.FilmStorage;
 
+import java.util.Comparator;
 import java.util.List;
+import java.util.Set;
+import java.util.stream.Collectors;
 
 @Service
 public class FilmService {
@@ -26,5 +30,18 @@ public class FilmService {
 
     public List<Film> getAllFilms() {
         return filmStorage.getAllFilms();
+    }
+
+    public void addLike(User user, Film film) {
+        film.getLikes().add(user.getId());
+    }
+    public void deleteLike(User user, Film film) {
+        film.getLikes().remove(user.getId());
+    }
+
+    public List<Film> getTopFilms() {
+        return filmStorage.getAllFilms().stream()
+                .sorted(Comparator.comparingInt(film -> film.getLikes().size()))
+                .collect(Collectors.toList());
     }
 }
